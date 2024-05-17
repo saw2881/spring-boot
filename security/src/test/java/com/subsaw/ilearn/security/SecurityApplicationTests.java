a2e29829613de3d87210cd9ade6fec1fc71b48a3
Subir Saw <saw2881@gmail.com>
2024-05-17 08:08:05 +0530

    spring security proeject setup

---
+++ security/src/test/java/com/subsaw/ilearn/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subsaw.ilearn.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
